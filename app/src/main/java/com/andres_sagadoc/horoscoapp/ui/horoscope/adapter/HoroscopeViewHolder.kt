4648be8421a0c8b202f30c1929e7825219fdaddf
Andrés Salgado <andressalgado41@gmail.com>
2024-10-30 13:06:37 -0500

    Add fun lambda and animations

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.andres_sagadoc.horoscoapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.andres_sagadoc.horoscoapp.databinding.ItemHoroscopeBinding
 import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo
@@ -9,8 +10,23 @@ class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = ItemHoroscopeBinding.bind(view)
 
-    fun render(horoscopeInfo: HoroscopeInfo) {
+    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit) {
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvHoroscope.text = binding.tvHoroscope.context.getString(horoscopeInfo.name)
+        binding.parent.setOnClickListener {
+            startRotationAnimation(
+                binding.ivHoroscope,
+                newLambda = { onItemSelected(horoscopeInfo) })
+        }
+    }
+
+    fun startRotationAnimation(view: View, newLambda: () -> Unit) {
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            withEndAction { newLambda() }
+            start()
+        }
     }
 }
