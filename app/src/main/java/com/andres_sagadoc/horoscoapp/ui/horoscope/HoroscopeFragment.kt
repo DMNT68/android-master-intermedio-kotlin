f1de911443e3c480f166f38a055c5b1142da8d36
Andrés Salgado <andressalgado41@gmail.com>
2024-10-30 13:06:55 -0500

    Add fun lambda

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.andres_sagadoc.horoscoapp.databinding.FragmentHoroscopeBinding
 import com.andres_sagadoc.horoscoapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,12 +37,15 @@ class HoroscopeFragment : Fragment() {
     }
 
     private fun initList() {
-        horoscopeAdapter = HoroscopeAdapter()
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+        })
         binding.rvHoroscope.apply {
             layoutManager = GridLayoutManager(context, 2)
             adapter = horoscopeAdapter
 
         }
+
     }
 
     private fun initUIState() {
