47393d319a2141946530bd59a92700eab9752d4f
Andrés Salgado <andressalgado41@gmail.com>
2024-10-30 13:05:57 -0500

    Add horoscope provider

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,37 +1,23 @@
 package com.andres_sagadoc.horoscoapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.andres_sagadoc.horoscoapp.data.providers.HoroscopeProvider
 import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo
-import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo.Aries
-import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo.Cancer
-import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo.Gemini
-import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeInfo.Taurus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeViewModel @Inject constructor() : ViewModel() {
+class HoroscopeViewModel @Inject constructor(horoscopeProvider: HoroscopeProvider) :
+    ViewModel() {
 
     private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
     val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
 
     init {
-        _horoscope.value = listOf(
-            Aries,
-            Taurus,
-            Gemini,
-            Cancer,
-            HoroscopeInfo.Leo,
-            HoroscopeInfo.Virgo,
-            HoroscopeInfo.Libra,
-            HoroscopeInfo.Scorpio,
-            HoroscopeInfo.Sagittarius,
-            HoroscopeInfo.Capricorn,
-            HoroscopeInfo.Aquarius,
-            HoroscopeInfo.Pisces
-        )
+
+        _horoscope.value = horoscopeProvider.getHoroscopes()
     }
 
 }
