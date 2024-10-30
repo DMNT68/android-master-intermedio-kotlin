b65a5f166f815fb87044703586b0cbe8ea003bd6
Andrés Salgado <andressalgado41@gmail.com>
2024-10-30 17:39:00 -0500

    Add findNavController()

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/horoscope/HoroscopeFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.andres_sagadoc.horoscoapp.databinding.FragmentHoroscopeBinding
 import com.andres_sagadoc.horoscoapp.ui.horoscope.adapter.HoroscopeAdapter
@@ -38,7 +38,9 @@ class HoroscopeFragment : Fragment() {
 
     private fun initList() {
         horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
-            Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity()
+            )
         })
         binding.rvHoroscope.apply {
             layoutManager = GridLayoutManager(context, 2)
