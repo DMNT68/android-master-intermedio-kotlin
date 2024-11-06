da9a205ea49286b0518e7a99d73182beeddab806
Andrés Salgado <andressalgado41@gmail.com>
2024-11-06 18:00:47 -0500

    Add values

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/detail/HoroscopeDetailState.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/detail/HoroscopeDetailState.kt
@@ -1,7 +1,9 @@
 package com.andres_sagadoc.horoscoapp.ui.detail
 
+import com.andres_sagadoc.horoscoapp.domain.model.HoroscopeModel
+
 sealed class HoroscopeDetailState {
     data object loading : HoroscopeDetailState()
     data class Error(val error: String) : HoroscopeDetailState()
-    data class Success(val data: String) : HoroscopeDetailState()
+    data class Success(val prediction: String, val sign: String, val horoscopeModel: HoroscopeModel) : HoroscopeDetailState()
 }
