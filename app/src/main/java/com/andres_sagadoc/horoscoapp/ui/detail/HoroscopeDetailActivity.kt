fc55543cc1fc4d30f2ed827d6ba512e871b9fc8f
Andrés Salgado <andressalgado41@gmail.com>
2024-11-06 16:34:31 -0500

    Add initUIState

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,8 +3,13 @@ package com.andres_sagadoc.horoscoapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.navArgs
 import com.andres_sagadoc.horoscoapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -12,10 +17,42 @@ class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
 
+    private val args: HoroscopeDetailActivityArgs by navArgs()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
 
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        HoroscopeDetailState.loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState() {
+        TODO("Not yet implemented")
+    }
+
+    private fun errorState() {
+        TODO("Not yet implemented")
+    }
+
+    private fun loadingState() {
+        TODO("Not yet implemented")
     }
 }
