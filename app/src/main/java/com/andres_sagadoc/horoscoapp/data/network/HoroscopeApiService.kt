1677b32917d7ba43d15d2064f6cf2ef6dacf90d2
Andrés Salgado <andressalgado41@gmail.com>
2024-11-07 12:01:48 -0500

    Add query param

--- app/src/main/java/com/andres_sagadoc/horoscoapp/data/network/HoroscopeApiService.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/data/network/HoroscopeApiService.kt
@@ -3,8 +3,9 @@ package com.andres_sagadoc.horoscoapp.data.network
 import com.andres_sagadoc.horoscoapp.data.network.response.PredictionResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface HoroscopeApiService {
     @GET("/{sign}")
-    suspend fun getHoroscope(@Path("sign") sing: String): PredictionResponse
+    suspend fun getHoroscope(@Path("sign") sing: String, @Query("lang") lang: String = "es"): PredictionResponse
 }
