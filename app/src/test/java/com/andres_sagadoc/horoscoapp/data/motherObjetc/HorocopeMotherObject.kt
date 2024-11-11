7f5d9d5a5b45fda35e987970e3cba5cf52f8b038
Andrés Salgado <andressalgado41@gmail.com>
2024-11-11 15:47:43 -0500

    Add mother object

---
+++ app/src/test/java/com/andres_sagadoc/horoscoapp/data/motherObjetc/HorocopeMotherObject.kt
@@ -0,0 +1,10 @@
+package com.andres_sagadoc.horoscoapp.data.motherObjetc
+
+import com.andres_sagadoc.horoscoapp.data.network.response.PredictionResponse
+
+object HorocopeMotherObject {
+
+    val anyReponse = PredictionResponse("date", "prediction", "sign")
+
+
+}
