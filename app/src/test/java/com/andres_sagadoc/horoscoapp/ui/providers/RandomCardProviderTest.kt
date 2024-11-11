47f399c3849eb6927711341f3a9a6787a03126d4
Andrés Salgado <andressalgado41@gmail.com>
2024-11-11 15:48:21 -0500

    Second Test

---
+++ app/src/test/java/com/andres_sagadoc/horoscoapp/ui/providers/RandomCardProviderTest.kt
@@ -0,0 +1,15 @@
+package com.andres_sagadoc.horoscoapp.ui.providers
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class RandomCardProviderTest{
+    @Test
+    fun `getRandomCard should return a random card`(){
+        val randomCard = RandomCardProvider()
+
+        val card = randomCard.getLucky()
+
+        assertNotNull(card)
+    }
+}
