29fd4ad656c407ea28194b03157ee00625e9acfa
Andrés Salgado <andressalgado41@gmail.com>
2024-11-11 10:14:48 -0500

    Add permissions management

--- app/src/main/java/com/andres_sagadoc/horoscoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/andres_sagadoc/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.andres_sagadoc.horoscoapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +14,41 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private var requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(requireContext(), "Acepta la camara", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (checkCameraPermission())
+            startCamera()
+        else
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+    }
+
+    private fun startCamera() {
+        TODO("Not yet implemented")
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            CAMERA_PERMISSION
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
